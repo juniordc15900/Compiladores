eeef9bb34d3e858b53456332945a4033b4c66b92
Alessandro Junior <juniordc15900@aluno.ufsj.edu.br>
2022-09-16 19:57:24 -0300

    fix tokens types in scanToken method

--- scanner.py
+++ scanner.py
@@ -1,5 +1,7 @@
 from token import Token
 
+from TokenType import TokenType
+
 
 class Scanner():
 
@@ -20,7 +22,7 @@ class Scanner():
     
     def addToken(self,tipo,start=0):
         text = self.source[start:self.current]
-        self.tokens.append(Token(tipo,text,literal,line=0))
+        self.tokens.append(Token(tipo,text,literal=0,line=0))
 
     def match(self,expected):
         if self.isAtEnd():
@@ -34,7 +36,7 @@ class Scanner():
     def peek(self):
         if(self.isAtEnd):
             return '\0'             #final de string em python
-        return source(self.current)
+        return self.source(self.current)
     
     def string(self):
         while(self.peek() != '"' and not self.isAtEnd):
@@ -43,7 +45,7 @@ class Scanner():
             else:
                 self.advance()
         if self.isAtEnd():
-            Lox.error(line, "Unterminated String")
+            Scanner.error(line, "Unterminated String")
             return
 
     def isDigit(c):
@@ -66,55 +68,55 @@ class Scanner():
         c = self.advance()
         
         if c == "(":
-            self.addToken("LEFT_PAREN")
+            self.addToken(TokenType.LEFT_PAREN)
         elif c == ")":
-            self.addToken("RIGHT_PAREN")
+            self.addToken(TokenType.RIGHT_PAREN)
         elif c == "{":
-            self.addToken("LEFT_BRACE")
+            self.addToken(TokenType.LEFT_BRACE)
         elif c == "}":
-            self.addToken("RIGHT_BRACE")
+            self.addToken(TokenType.RIGHT_BRACE)
         elif c == ",":
-            self.addToken("COMMA")
+            self.addToken(TokenType.COMMA)
         elif c == ".": 
-            self.addToken("DOT")
+            self.addToken(TokenType.DOT)
         elif c == "-":  
-            self.addToken("MINUS")
+            self.addToken(TokenType.MINUS)
         elif c == "+":  
-            self.addToken("PLUS")
+            self.addToken(TokenType.PLUS)
         elif c == ";":  
-            self.addToken("SEMICOLON")
+            self.addToken(TokenType.SEMICOLON)
         elif c == "*":  
-            self.addToken("STAR")
+            self.addToken(TokenType.STAR)
         elif c == "!":
-            self.addToken("BANG_EQUAL" if self.match("=") else "BANG")
+            self.addToken(TokenType.BANG_EQUAL if self.match("=") else TokenType.BANG)
         elif c == "=":
-            self.addToken("EQUAL_EQUAL" if self.match("=") else "EQUAL")
+            self.addToken(TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL)
         elif c == "<":
-            self.addToken("LESS_EQUAL" if self.match("=") else "EQUAL")
+            self.addToken(TokenType.LESS_EQUAL if self.match("=") else TokenType.EQUAL)
         elif c == ">":
-            self.addToken("GREATER_EQUAL" if self.match("=") else "EQUAL")
+            self.addToken(TokenType.GREATER_EQUAL if self.match("=") else TokenType.EQUAL)
         elif c == "/":
-            if match('/'):
+            if self.match('/'):
                 while (self.peek() != '/n' and not self.isAtEnd()):
                     self.advance()
             else:
-                self.addToken("SLASH")
+                self.addToken(TokenType.SLASH)
         elif c == "/n":
             line+=1
         elif c == '"':
             self.string()
         else:
             if self.isDigit(c):
-                number()
+                self.number()
             else: 
                 line=0
             Scanner.error(line, "Unespected character")
 
     
-    def scanTokens(self,start=0,line=1):
+    def scanTokens(self,start=0,line=0):
         while not self.isAtEnd():
             start = self.current
             self.scanToken()
-            self.tokens.append(Token("EOF","",None,line))    
-            return self.tokens
+        self.tokens.append(Token(TokenType.EOF,"",None,line))    
+        return self.tokens
         #return list of tokens
