911cfcda71f12d89935e5ca91b4fb37442ad5abd
juniordc15900 <juniordc15900@gmail.com>
2022-10-18 14:28:18 -0300

    parser tratamento de exceção

--- src/lox/Parser.py
+++ src/lox/Parser.py
@@ -1,30 +1,57 @@
+from parser import ParserError
+from tokenn import Tokenn
+from lox import Lox
+from TokenType import TokenType
 class Parser:
 
-    def match(types: TokenType):
+    def __init__(self):
+        self.parseError = ParserError()
+        self.tokens = []
+        self.current = 0
+        
+    def match(self,types):
         for type in types:
-            if check(type):
-                advance()
+            if self.check(type):
+                self.advance()
                 return True
         return False
     
-    def check(type:TokenType):
-        if isAtEnd():
+    def check(self,type:TokenType):
+        if self.isAtEnd():
             return False
-        return peek().type == type
+        return self.peek().type == type
     
     def advance(self):
-        if isAtEnd():
+        if self.isAtEnd():
             current += 1
-        return previous()
+        return self.previous()
     
-    def isAtEnd():
-        return peek().type == 'EOF'
+    def isAtEnd(self):
+        return self.peek().type == 'EOF'
     
     def peek():
-        return Tokkens.get(current)
+        return Tokenn.get(current)
     
     def previous():
-        return Tokkens.get(current-1)
+        return Tokenn.get(current-1)
     
     def comparision():
         return None
+    
+    def consume(self, type : TokenType, message):
+        if self.check(type):
+            return self.advance()
+        raise self.error(self.peek(), message)
+
+    def error(self, token: Tokenn, message):
+        Lox.error(token,message)
+        return self.parseError
+    
+    def synchronize(self):
+        self.advance()
+        while (self.isAtEnd()):
+            if self.previous().type == TokenType.SEMICOLON: return
+            
+
+
+    
